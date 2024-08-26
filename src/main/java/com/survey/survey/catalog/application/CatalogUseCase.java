116591d343eb3886fe6e246289ea737c663581a8
CristopherBuitrago <cristopherbuitrago0907@gmail.com>
2024-08-26 16:25:23 -0500

    feat: :memo: Add Catalog Entity

---
+++ src/main/java/com/survey/survey/catalog/application/CatalogUseCase.java
@@ -0,0 +1,5 @@
+package com.survey.survey.catalog.application;
+
+public class CatalogUseCase {
+
+}
---
+++ src/main/java/com/survey/survey/catalog/infrastructure/out/CatalogRepository.java
@@ -0,0 +1,5 @@
+package com.survey.survey.catalog.infrastructure.out;
+
+public interface CatalogRepository {
+
+}
