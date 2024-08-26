f112d9ee33f1a0f53970a51e32418192e327e0d9
santilaguado <santiagolaguado57@gmail.com>
2024-08-26 15:44:11 -0500

    feat(Add Survey): :memo: Add Survey Entity

---
+++ src/main/java/com/survey/survey/survey/domain/entity/Survey.java
@@ -0,0 +1,5 @@
+package com.survey.survey.survey.domain.entity;
+
+public class Survey {
+
+}
---
+++ src/main/java/com/survey/survey/survey/infrastructure/web/SurveyController.java
@@ -0,0 +1,5 @@
+package com.survey.survey.survey.infrastructure.web;
+
+public class SurveyController {
+
+}
