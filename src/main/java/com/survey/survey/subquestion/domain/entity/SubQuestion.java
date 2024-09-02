8553e7042a94d16381981f526a53361637e5cca4
CristopherBuitrago <cristopherbuitrago0907@gmail.com>
2024-09-01 21:54:40 -0500

    feat: :necktie: add SubQuestion entity

--- src/main/java/com/survey/survey/subquestion/domain/entity/SubQuestion.java
+++ src/main/java/com/survey/survey/subquestion/domain/entity/SubQuestion.java
@@ -1,58 +1,45 @@
-package com.survey.survey.question.domain.entity;
+package com.survey.survey.subquestion.domain.entity;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
-import com.survey.survey.subquestion.domain.entity.SubQuestion;
+import com.survey.survey.question.domain.entity.Question;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PostPersist;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name="questions")
 @NoArgsConstructor
 @Getter
 @Setter
-public class Question {
+@Table(name="sub_questions")
+public class SubQuestion {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private int id;
 
-    @Column(nullable=false)
-    @NotBlank(message="The field cannot be blank!")
-    private String questionNumber;
+    @ManyToOne
+    @JoinColumn(name="question_id")
+    private Question question;
 
-    @Column(nullable=false)
-    @NotBlank(message="The field cannot be blank!")
-    private String responseType;
+    @Column
+    private String subquestionText;
 
-    @Column(nullable=false)
-    @NotBlank(message="The field cannot be blank!")
-    private String commentQuestion;
-
-    @Column(nullable=false)
-    @NotBlank(message="The field cannot be blank!")
-    private String questionText;
-    
     @Column(columnDefinition="TIMESTAMP", nullable=false, updatable=false)
     private LocalDateTime createdAt;
-    
+
     @Column(columnDefinition="TIMESTAMP", nullable=false, updatable=false)
     private LocalDateTime updatedAt;
-
-    @OneToMany(mappedBy="question")
-    private List<SubQuestion> subQuestions;
 
     @PrePersist
     protected void onCreate() {
